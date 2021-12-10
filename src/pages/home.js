7ad59b48009c3188de1a4fdf5e7ea84e91e569ce
Evgeniy <85104881+EVG777-prog@users.noreply.github.com>
2021-12-10 22:28:48 +0200

    create HTML homepage and footer

---
+++ src/pages/home.js
@@ -0,0 +1,8 @@
+const homePageHTML = `
+  <div class="home-page">
+    <img class="home-page__ball home-page__ball-1" src="/assets/1.png" alt="ball1">
+    <img class="home-page__ball home-page__ball-2" src="/assets/2.png" alt="ball2">
+    <div class="home-page__text">Помогите бабушке <br> нарядить елку</div>
+    <div class="home-page__btn-start">Начать</div>
+  </div>
+`;
