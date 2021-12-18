7bc7bb13f69eaaf52f248410b098234ba18505fa
Evgeniy <85104881+EVG777-prog@users.noreply.github.com>
2021-12-18 17:24:07 +0200

    create select.css

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,54 @@
+import * as noUiSlider from 'nouislider';
+import 'nouislider/dist/nouislider.css';
+
 console.log('I am working');
+
+
+
+const sliderCount: noUiSlider.target = document.querySelector('.range-count')!;
+const sliderYear: noUiSlider.target = document.querySelector('.range-year')!;
+
+
+
+if (sliderCount) {
+  noUiSlider.create(sliderCount, {
+    start: [1, 12],
+    step: 1,
+    connect: true,
+    range: {
+      'min': 1,
+      'max': 12
+    },
+  });
+
+  if (sliderCount.noUiSlider) {
+    sliderCount.noUiSlider.on('update', (el) => {
+      console.log(el);
+      document.querySelector('.select__count .select__text.start')!.textContent = `${Math.round(+el[0])}`;
+      document.querySelector('.select__count .select__text.end')!.textContent = `${Math.round(+el[1])}`;
+
+    });
+  }
+}
+if (sliderYear) {
+  noUiSlider.create(sliderYear, {
+    start: [1940, 2020],
+    step: 1,
+    connect: true,
+    range: {
+      'min': 1940,
+      'max': 2020
+    }
+  });
+
+  if (sliderYear.noUiSlider) {
+    sliderYear.noUiSlider.on('update', (el) => {
+      console.log(el);
+      document.querySelector('.select__year .select__text.start')!.textContent = `${Math.round(+el[0])}`;
+      document.querySelector('.select__year .select__text.end')!.textContent = `${Math.round(+el[1])}`;
+
+    });
+  }
+}
+
+
--- src/pages/select.js
+++ src/pages/select.js
@@ -1,52 +1,98 @@
 const selectPage = `
-<div class="select">
+<div class="container select">
 <div class="select__setting">
-    <div class="select__sort">
+    <div class="container select__sort">
         <p>Сортировка</p>
         <select name="select__sort-metod" id="sort-metod">
-            <option value="a-to-z">По названию от "А" до "Я"</option>
-            <option value="z-to-a">По названию от "Я" до "А"</option>
-            <option value="count-to-bigger">По количеству по возрастанию</option>
-            <option value="count-to-smaller">По количеству по убыванию</option>
-        </select>
+        <option value="a-to-z">По названию от "А" до "Я"</option>
+        <option value="z-to-a">По названию от "Я" до "А"</option>
+        <option value="count-to-bigger">По количеству по возрастанию</option>
+        <option value="count-to-smaller">По количеству по убыванию</option>
+    </select>
     </div>
-    <div class="select__form">
+    <div class="container select__form">
         <p>Форма:</p>
-        <div class="form ball"></div>
+        <div class="form ball active"></div>
         <div class="form bell"></div>
         <div class="form cone"></div>
         <div class="form star"></div>
         <div class="form figure"></div>
     </div>
-    <div class="select__color">
+    <div class="container select__color">
         <p>Цвет:</p>
         <div class="color white"></div>
-        <div class="color yellow"></div>
+        <div class="color yellow active"></div>
         <div class="color red"></div>
         <div class="color blue"></div>
         <div class="color green"></div>
     </div>
-    <div class="select__size">
+    <div class="container select__size">
         <p>Размер:</p>
-        <div class="size big"></div>
+        <div class="size big active"></div>
         <div class="size medium"></div>
         <div class="size small"></div>
     </div>
-    <div class="select__favorite">
+    <div class="container select__favorite">
         <p>Только любимые:</p>
-        <input type="checkbox">
+        <input id="chbx-favor" class="checkbox-favorite" type="checkbox">
+        <label for="chbx-favor"></label>
     </div>
-    <div class="select__count">
+    <div class="container select__count">
         <p>Количество экземпляров:</p>
-        <div class="select__range range-count"></div>
+        <div class="container select__count-line">
+            <div class="select__text start">1</div>
+            <div id="range-count" class="select__range range-count"></div>
+            <div class="select__text end">12</div>
+        </div>
+
     </div>
-    <div class="select__year">
+    <div class="container select__year">
         <p>Год приобретения:</p>
-        <div class="select__range range-year"></div>
+        <div class="container select__count-line">
+            <div class="select__text start">1940</div>
+            <div id="range-year" class="select__range range-year"></div>
+            <div class="select__text end">2020</div>
+        </div>
+
     </div>
-    <div class="select__reset"></div>
+    <div class="container select__reset">Сброс фильтров</div>
 
 </div>
-<div class="select__toys"></div>
+<div class="container select__toys">
+    <div class="select__card">
+        <div class="select__card-name">Зеленый шар с цветами</div>
+        <div class="container">
+            <div class="select__card-img">
+                <img src="/assets/toys/1.png" alt="picture">
+            </div>
+            <div class="select__card-text">
+                <p>Количество: 5</p>
+                <p>Год покупки: 2000</p>
+                <p>Форма: шар</p>
+                <p>Цвет: зеленый</p>
+                <p>Размер: большой</p>
+                <p>Любимая: нет</p>
+            </div>
+
+        </div>
+    </div>
+    <div class="select__card active">
+        <div class="select__card-name">Зеленый шар с цветами</div>
+        <div class="container">
+            <div class="select__card-img">
+                <img src="/assets/toys/1.png" alt="picture">
+            </div>
+            <div class="select__card-text">
+                <p>Количество: 5</p>
+                <p>Год покупки: 2000</p>
+                <p>Форма: шар</p>
+                <p>Цвет: зеленый</p>
+                <p>Размер: большой</p>
+                <p>Любимая: нет</p>
+            </div>
+
+        </div>
+    </div>
+</div>
 </div>
 `;
